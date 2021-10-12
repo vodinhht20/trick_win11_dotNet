c209d70f4cd17da0f7282b32d146571c3667dee5
vodinhht20 <vodinh2000ht@gmail.com>
2021-10-12 10:41:42 +0700

    up source code

---
+++ 01.cmd
@@ -0,0 +1,5 @@
+DISM /Online /Cleanup-Image /RestoreHealth /source:WIM:X:\Sources\Install.wim:1 /LimitAccess
+Dism.exe /online /enable-feature /featurename:NetFX3 /source:C:\ /LimitAccess
+powershell Start-Process https://www.youtube.com/channel/UCSfR-KO8UkS5WfDbMUsdpww
+powershell Start-Process https://www.youtube.com/c/MatrixSquad?sub_confirmation=1
+
